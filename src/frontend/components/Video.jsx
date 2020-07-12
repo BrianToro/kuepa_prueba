1b209ee4efcffbe15bb5fd1fc19615adde223a70
Brian Toro <brianhorrors@hotmail.com>
2020-07-11 20:01:00 -0500

    chenge the video

--- src/frontend/components/Video.jsx
+++ src/frontend/components/Video.jsx
@@ -5,7 +5,7 @@ import '../assets/styles/components/Video.scss';
 const Video = () => {
     return (
         <div className="container-video">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/y8OnoxKotPQ" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/jlvK_aQkp3Y" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
         </div>
     )
 }
